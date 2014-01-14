b5df6c9ed660faaadefe55070b60e01afd845b10
Jacob Carey <jaccarey@deloitte.com>
2014-01-14 15:26:28 -0500

    Started chap_5 p5

---
+++ ex_5/p5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+double* Multiply(double M[][], double N[][]);
+
+int main(int argc, char* argv[])
+{
+	double A[2][2] = {{1.0, 0.0}, {0.0, 1.0}};
+	double B[2][2] = {{2.0, 3.0}, {4.5, 7.0}};
+
+	return 0;
+}
+
+double* Multiply(double M[][], double N[][])
+{
+
+}
+
