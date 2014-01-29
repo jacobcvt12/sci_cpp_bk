9b43da9312e08da74cd0afe6670f9409d5d336dd
Jacob Carey <jacobcvt12@gmail.com>
2014-01-28 19:53:39 -0500

    another quick commit

--- chap_6/ComplexNumber.cpp
+++ chap_6/ComplexNumber.cpp
@@ -17,8 +17,38 @@ ComplexNumber::ComplexNumber(double x, double y)
 }
 
 // Method for calculating the modulus of a complex number
-ComplexNumber::CalculateModulus() const
+double ComplexNumber::CalculateModulus() const
 {
     return sqrt(mRealPart*mRealPart +
                 mImaginaryPart*mImaginaryPart);
+}
+
+// Method for computing the argument of a complex number
+
+double ComplexNumber::CalculateArgument() const
+{
+    return atan2(mImaginaryPart, mRealPart);
+}
+
+// Method for raising complex number to the power n using De Moivre's theorem
+// first complex number must be converted to polar form
+
+ComplexNumber ComplexNumber::CalculatePower(double n) const
+{
+    double modulus = CalculateModulus();
+    double argument = CalculateArgument();
+    double mod_of_result = pow(modulus, n);
+    double arg_of_result = argument*n;
+    double real_part = mod_of_result*cos(arg_of_result);
+    double imag_part = mod_of_result*sin(arg_of_result);
+    ComplexNumber z(real_part, imag_part);
+    return z;
+}
+
+// Overloading the = (assignment) operator
+ComplexNumber& ComplexNumber::operator=(const ComplexNumber& z)
+{
+    mRealPart=z.mRealPart;
+    mImaginaryPart=z.mImaginaryPart;
+    return *this;
 }
