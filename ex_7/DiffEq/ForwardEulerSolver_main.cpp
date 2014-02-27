c63841031929c2f10401d533ce735027102b4766
Jacob Carey <jacob.carey@marriott.com>
2014-02-27 16:38:38 -0500

    Still working on 7.3... compiler issues?

---
+++ ex_7/DiffEq/ForwardEulerSolver_main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+#include "ForwardEulerSolver.hpp"
+
+double diffEq(double Y, double T)
+{
+    return 1 + T;
+}
+
+double SolDiffEq(double t)
+{
+    return (pow(t, 2.0) + 2.0*t + 4.0)/2.0;
+}
+
+int main()
+{
+    ForwardEulerSolver easy_diff_eq;
+    easy_diff_eq.SetStepSize(100);
+    //easy_diff_eq.SetTimeInterval(0, 1);
+    //easy_diff_eq.SetInitialValue(2);
+
+    double actual;
+    actual = SolDiffEq(1);
+
+    std::cout << "Actual solution at time t=1 is = " << actual << "\n";
+
+    return 0;
+}
